218d0a7d4f7120e384a83919437cca2cce2a010b
Bramonowangsa <101787984+Bramonowangsa@users.noreply.github.com>
2022-11-08 10:35:17 +0700

    Add files via upload

---
+++ Tendik.java
@@ -0,0 +1,25 @@
+public class Tendik extends Karyawan{
+    protected int total;
+
+    // protected int totalGaji;
+    @Override
+    public int gaji() {
+        return super.gaji();
+    }
+
+    public int gajiTotal() {
+        return super.gaji();
+    }
+
+    public int lembur(int jam) {
+        // gaji();
+        // int total;
+        total = 50000 * jam;
+        return total;
+    }
+
+    // overloading
+    public int gajiTotal(int jam) {
+        return gaji + total;
+    }
+}
